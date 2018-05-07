4beccbd48f219ad8f54ee3d85ad2bc7a96e94788
kysevenle <kysevenle@gmail.com>
2018-05-07 17:04:38 -0600

    added area_regex and started working on script to check areas for line notes in services

--- scripts/__init__.py
+++ scripts/__init__.py
@@ -1,1 +1,2 @@
 import work_package.scripts.update_txts
+import work_package.scripts.check_area_lines
---
+++ scripts/check_area_lines.py
@@ -0,0 +1,7 @@
+import work_package
+import re
+
+def main():
+    services = work_package.api_calls.get_services()
+     re.fullmatch(work_package.area_regex.north_c_ips, ip)}
+    print(lines)
