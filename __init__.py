0dfecd0f67e0a03eb06d690451e7ab72ebbf0d2e
kysevenle <kysevenle@gmail.com>
2018-05-07 20:58:13 -0600

    finished skeleton of check and tested with north c and timp A

--- __init__.py
+++ __init__.py
@@ -2,3 +2,4 @@ import work_package.api_calls
 import work_package.api_creds
 import work_package.api_filters
 import work_package.scripts
+import work_package.area_regex
--- scripts/check_area_lines.py
+++ scripts/check_area_lines.py
@@ -3,5 +3,15 @@ import re
 
 def main():
     services = work_package.api_calls.get_services()
-     re.fullmatch(work_package.area_regex.north_c_ips, ip)}
-    print(lines)
+    lines = set()
+    for service in services:
+        for ip in service['ipRanges']:
+            if re.fullmatch(work_package.area_regex.timp_a, ip):
+                if service['note'] == None:
+                    print('Client - ' + str(service['clientId']) + ' -- has no note')
+                else:
+                    lines.add(service['note'])
+    lines
+
+if __name__ == "__main__":
+    main()
